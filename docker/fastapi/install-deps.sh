6c66a4feda09ab52823575a5a11c5f5aea70d80c
Bjørn Snoen <bjorn.snoen@visma.com>
2020-11-15 19:11:46 +0100

    (fix) Remove uwsgi from docker install

--- docker/fastapi/install-deps.sh
+++ docker/fastapi/install-deps.sh
@@ -1,8 +1,8 @@
+set -e
 apt-get update -y
 apt-get install gcc supervisor sqlite3 -y
 pip install pipenv
 PIPENV_VENV_IN_PROJECT=1 pipenv install
-PIPENV_VENV_IN_PROJECT=1 pipenv install uwsgi
 apt-get install nginx -y
 apt-get remove gcc -y
 apt-get autoremove -y
