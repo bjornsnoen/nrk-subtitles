f1607f9715893f70d608f376f7a783c37e63e105
Bjørn Snoen <bjorn.snoen@visma.com>
2020-11-15 23:55:34 +0100

    (feature) Dockerized debug with on/off in docker-compose

--- docker/fastapi/install-deps.sh
+++ docker/fastapi/install-deps.sh
@@ -1,9 +1,8 @@
 set -e
 apt-get update -y
-apt-get install gcc supervisor sqlite3 -y
+apt-get install gcc supervisor sqlite3 libnginx-mod-http-lua nginx -y
 pip install pipenv
 PIPENV_VENV_IN_PROJECT=1 pipenv install
-apt-get install nginx -y
 apt-get remove gcc -y
 apt-get autoremove -y
 
--- skam/app.py
+++ skam/app.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 """ Runner for the nrk subs app """
+from os import getenv
+
 import uvicorn  # type: ignore
 
 from skam.server.server import app
+
+if getenv("DEBUG") == "1":
+    import debugpy  # type: ignore
+
+    host = getenv("DEBUG_LISTEN_HOST")
+    port = getenv("DEBUG_LISTEN_PORT")
+    if host is not None and port is not None:
+        debugpy.listen((host, int(port)))
+    else:
+        debugpy.listen(5678)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
